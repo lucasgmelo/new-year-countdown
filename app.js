4586d3da6d33eab5ff1299a9041de51cfc22bfd3
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2020-12-31 15:44:59 -0300

    feat(app): atualize tags

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const countDate = new Date("jan 1 2021 00:00:00").getTime();
 console.log(newYear);
 
-const hour = document.getElementById('one');
-const minute = document.getElementById('two');
-const seconds = document.getElementById('three');
+const hour = document.getElementById('html-hour');
+const minute = document.getElementById('html-min');
+const seconds = document.getElementById('html-sec');
 
 function newYear() {
     let now = new Date().getTime();
@@ -18,6 +18,7 @@ function newYear() {
         let h = Math.floor((gap % (day)) / (hour));
         let m = Math.floor((gap % (hour)) / (minute));
         let s = Math.floor((gap % (minute)) / second);
+        console.log(s, m, h)
 
         seconds.innerHTML = `${s}s`;
         minute.innerHTML = `${m}min`;
